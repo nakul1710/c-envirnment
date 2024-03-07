617b3f13f2b1dd2921ad5f04afcde7633da35018
Nakul <nakulyadav1710@gmail.com>
2024-03-07 11:08:43 +0530

    lab0/main.c

--- jecrc/lab0/main.c
+++ jecrc/lab0/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+ void main()
+{
+  /*Que:1*/
+  printf("Name          :Nakul Yadav\n");
+  printf("Father's Name :Ram Yadav\n");
+  printf("Mother's Name :Sarita Yadav\n");
+  printf("Address       :17 House no\n");
+  printf("               Income tax colony\n");
+  printf("               Durgapura\n");
+  printf("Mobile        :9116971025\n");
+  printf("Email         :nakulyadav1710@gmail.com\n");
+
+/*Que:2*/
+printf("          *       \n");
+printf("         ***      \n");
+printf("        *****     \n");
+printf("       *******   \n");
+
+
+/*Que:3*/
+  printf("*           * * * * * * *\n");
+  printf("*           *            \n");
+  printf("*           *            \n");
+  printf("* * * * * * * * * * * * *\n");
+  printf("            *           *\n");
+  printf("            *           *\n");
+  printf("* * * * * * *           *");
 }
+
