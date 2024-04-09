493132939c9a423ed6c06ab1b3d1ba3af0ceb5d6
Nakul <nakulyadav1710@gmail.com>
2024-04-10 00:02:45 +0530

    jecrc-lab1

--- jecrc/lab1/main.c
+++ jecrc/lab1/main.c
@@ -1,4 +1,6 @@
 // write a C programme to add to numbers
+
+
 // #include <stdio.h>
 // void main()
 // {
@@ -14,22 +16,47 @@
 // }
 
 //write a C programme to add , Substract, Multiply, Divide
+
+
+// #include <stdio.h>
+// void main()
+// {
+//     int num1, num2 , add, subs, Multiply, quo, rem;
+//     printf("Enter num1: ");
+//     scanf("%d",&num1);
+//     printf("Enter num2: ");
+//     scanf("%d",&num2);
+//     add = num1 + num2;
+//     subs = num1 - num2;
+//     Multiply = num1*num2;
+//     quo = num1/num2;
+//     rem = num1 % num2;
+//     printf("The sum is %d\n",add);
+//     printf("The substraction is%d\n",subs);
+//     printf("The Multiply is %d\n",Multiply);
+//     printf("The quotient is %d\n",quo);
+//     printf("The remainder is %d\n",rem);
+// }
+
+//Write a C programme to calculate area of rectangle
+// #include <stdio.h>
+// void main(){
+//     int Length,Breadth,area;
+//     printf("Enter the Length = ");
+//     scanf("%d",&Length);
+//     printf("Enter the Breadth = ");
+//     scanf("%d",&Breadth);
+//     area = Length*Breadth;
+//     printf("The area is %d",area);
+//     getch();
+// }
+
+//write a C programme to calculate the area of the circle.
 #include <stdio.h>
-void main()
-{
-    int num1, num2 , add, subs, Multiply, quo, rem;
-    printf("Enter num1: ");
-    scanf("%d",&num1);
-    printf("Enter num2: ");
-    scanf("%d",&num2);
-    add = num1 + num2;
-    subs = num1 - num2;
-    Multiply = num1*num2;
-    quo = num1/num2;
-    rem = num1 % num2;
-    printf("The sum is %d\n",add);
-    printf("The substraction is%d\n",subs);
-    printf("The Multiply is %d\n",Multiply);
-    printf("The quotient is %d\n",quo);
-    printf("The remainder is %d\n",rem);
+void main(){
+    float r,area;
+    printf("Enter r = ");
+    scanf("%f",&r);
+    area = 3.14*r*r;
+    printf("The area of the circle is %f\n", area);
 }
