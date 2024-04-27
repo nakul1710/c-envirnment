dd6e69b064447b7279cef4ed35e1d3a05794c574
Nakul <nakulyadav1710@gmail.com>
2024-04-27 23:20:49 +0530

    add  que in lab2

--- jecrc/lab2/main.c
+++ jecrc/lab2/main.c
@@ -27,3 +27,93 @@
 //     printf("%d is odd", num);
 //   }
 // }
+
+
+//Write a C program to input two numbers and find the gratest.
+
+// if(a>b){
+//     printf("a is gratest");
+
+// }
+// else     {
+//     printf("b is gratest");
+// }
+
+
+
+
+
+// write a C program  to input a age of a person and check wheather he/she is elligibal for voating.
+
+
+// if(age>=18){
+//   printf("Elligibal for voating");
+// }
+// else {
+//     printf("Not elligibal for voating");
+// }
+
+
+// write a c program to input year and check wheather it is a leap year or not
+/*if(year%4 == 0){
+   printf("%d is a leap year");
+}
+else {
+    printf("It is not a leap year");
+}*/
+
+
+//write a c progra to input wheather it is  positive or negative
+/*
+if(num>0){
+    printf("%d is positive");
+}
+else {
+    printf("%d is negative");
+}
+
+*/
+
+
+// write a c program to input to find the greatest between  3 numbers.
+
+/*
+if(a>=b && a>=c){
+    printf("%d is the greatest",a);
+}
+else if(b>=a && b>=c){
+    printf("%d is gratest",b);
+}
+else{
+    printf("%d is gratest", num3);
+}
+*/
+
+
+// write a c program to input a year  and check wheather it is a leap year or not
+
+
+/*
+if(Year%4 == 0){
+    if(year%100==0){
+        if(year%400==0){
+            printf("%d is a leap year",year);
+        }
+        else {
+            printf("%d is not a leap year",year);
+        }
+    }
+    else {
+        printf("%d is Not a leap year\n",year);
+    }
+    else {
+        printf("%d is not a leap year\n",year);
+    }
+}
+
+*/
+
+
+
+
+
