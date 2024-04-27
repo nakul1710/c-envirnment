0fa455f2e546dec6aaf82b5444e2210444ec4c5f
Nakul <nakulyadav1710@gmail.com>
2024-04-27 22:59:05 +0530

    add 2 que in lab2

---
+++ jecrc/lab2/main.c
@@ -0,0 +1,29 @@
+// Write a c program to swap a number without using third variable.
+// #include <stdio.h>
+// void main() {
+//     int a,b;
+//     clrscr();
+//     printf("Enter the first number: ");
+//     scanf("%d",&a);
+//     printf("Enter second number: ");
+//     scanf("%d",b);
+//     a = a+b;
+//     b = a-b;
+//     a = a-b;
+//     printf("After swapping a = %d\n b = %d",a,b);
+//     getch();
+// }
+
+
+//write a c program to input a number and check wheather it is odd ar even.
+// void main(){
+//   int num;
+//   printf("Enter the number: ");
+//   scanf("%d",num);
+//   if(num%2 == 0){
+//     printf("%d is even. \n",num);
+//   }
+//   else {
+//     printf("%d is odd", num);
+//   }
+// }
